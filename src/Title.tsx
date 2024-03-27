e33da336aa81c42f6023fb902dc10f2caa3ab257
pannachow <chowpanna@gmail.com>
2024-03-27 20:31:04 +0100

    Improve title readability

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
     <section className="p-10">
-      <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
         Dassembly
       </h1>
     </section>
