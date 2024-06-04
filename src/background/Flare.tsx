b41d73e7c26943462e2029441e5880bc0b76265e
pannachow <chowpanna@gmail.com>
2024-06-04 13:19:07 +0200

    Update prettier and reformat

--- src/background/Flare.tsx
+++ src/background/Flare.tsx
@@ -3,7 +3,7 @@ import background from "../assets/background.png";
 export default function Flare() {
   return (
     <div className="pointer-events-none absolute inset-0 z-20 flex justify-center overflow-hidden">
-      <div className="flex w-[108rem] flex-none justify-end ">
+      <div className="flex w-[108rem] flex-none justify-end">
         <img
           src={background}
           alt=""
